19fe645407ac6c968938fb710cd4630d082e99e8
Oscar Flores <oofloresg@gmail.com>
2013-05-04 13:45:03 -0600

    Inicio

---
+++ Fraccion/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConstructorDefault()
+        {
+            FRACIONES F = new FRACIONES();
+            int numerador = F.Numerador;
+            int denominador = F.Denominador;
+            bool b = (numerador == 1) && (denominador == 1);
+            Assert.AreEqual(true, b);
+
+        }
+        [TestMethod]
+        public void TestSumarFraciones()
+        {
+            FRACIONES f1 = new FRACIONES(2,3);
+            FRACIONES F2 = new FRACIONES(1, 3);
+            FRACIONES NuevaFraccionExpected = new FRACIONES(3, 3);
+            FRACIONES resultado = new FRACIONES();
+            resultado.Sumar(f1, F2);
+
+            bool b = (resultado.Numerador == 3) && (resultado.Denominador == 3);
+                     
+            Assert.AreEqual(true, b);
+        }
+
+        [TestMethod]
+        public void TestDenominadorNoCero()
+        {
+            FRACIONES F = new FRACIONES();
+
+            try
+                {
+
+                    F.Denominador = 0;
+                
+                }
+            catch(DivideByZeroException O)
+
+                {
+
+                return; 
+        }
+
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestSumarCalc()
+        {
+            Calc c = new Calc();
+            int valor1 = 10;
+            int valor2 = 10;
+            int resultado = c.sumar(valor1, valor2);
+            Assert.AreEqual(20, resultado); 
+        }
+    }
+}
+
+
+
---
+++ Fraccion/WindowsFormsApplication1/FRACIONES.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class FRACIONES
+    {
+
+        public int Numerador { get; set; }
+
+        public int Denominador { get; set; }
+
+        public FRACIONES()
+             {
+
+            this.Numerador = 1;
+            this.Denominador = 1;
+            }
+            public FRACIONES(int num, int den)
+            {
+                this.Numerador = num;
+                this.Denominador = den;
+
+            }
+
+
+            public void Sumar(FRACIONES f1, FRACIONES F2)
+            {
+                throw new NotImplementedException();
+            }
+    }
+}
