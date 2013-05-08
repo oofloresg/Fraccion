a6c0303301a488ca542c79c284ef9468054fbe73
Oscar Flores <oofloresg@gmail.com>
2013-05-08 01:07:10 -0600

    millaje
    test

--- Fraccion/WindowsFormsApplication1/Calc.cs
+++ Fraccion/WindowsFormsApplication1/Calc.cs
@@ -31,5 +31,9 @@ namespace WindowsFormsApplication1
             return valor1 * valor2 / valor1;
         }
 
+        public int millaje(int valor1, int valor2)
+        {
+            return valor1 * valor2 * valor1;
+        }
     }
 }
